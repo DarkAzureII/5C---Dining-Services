39404eba5842a72759d38ba4856d62c1a39e413c
DarkAzureII <2304961@students.wits.ac.za>
2024-10-06 09:20:57 +0200

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "cypress";
+import codeCoverageTask from '@cypress/code-coverage/task';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('@cypress/code-coverage/task')(on, config)
+      codeCoverageTask(on, config);
       // include any other plugin code...
 
       // It's IMPORTANT to return the config object

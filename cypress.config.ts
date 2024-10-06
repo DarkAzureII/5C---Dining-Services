1e676e1ee0e636462bd023e843bde32620630f30
DarkAzureII <2304961@students.wits.ac.za>
2024-10-06 09:15:37 +0200

    not much

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config
     },
     supportFile: 'cypress/support/e2e.ts',
     baseUrl: 'http://localhost:5173', // Your app's URL

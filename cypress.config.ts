6591bc283aaf0d26ba94f9f54114a0137982a56c
DarkAzureII <2304961@students.wits.ac.za>
2024-10-06 09:35:09 +0200

    More

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,12 +4,7 @@ import codeCoverageTask from '@cypress/code-coverage/task';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       codeCoverageTask(on, config);
-      // include any other plugin code...
-
-      // It's IMPORTANT to return the config object
-      // with any changed environment variables
       return config
     },
     supportFile: 'cypress/support/e2e.ts',
